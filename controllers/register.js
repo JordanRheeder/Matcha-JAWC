7b16f6aa4e5b3fe5e5a9f2f682cb3e860b33ecb8
JordanRheeder <JordanRheeder@gmail.com>
2019-12-03 16:10:09 +0200

    Created our controller to register the users and fixed the bug that I assumed it was in scope of our userSchema, turns out it needs to be exported.

--- controllers/register.js
+++ controllers/register.js
@@ -1,11 +1,17 @@
 const user = require("../models/user.js");
+const mongoose = require('mongoose');	
+var uri = process.env.URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); 
+var db=mongoose.connection;
 
 module.exports = {
-	register: function registerNewUser(req, res) {
+	register: async function registerNewUser(req, res) {
 	try {
-		const hashedPassword = bcrypt.hash(req.body.password, 10)
-		// Grabbing the inputs form data 
-		var data = new userSchema({
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		var data = new user({
 			firstname: req.body.fname,
 			lastname: req.body.sname,
 			email: req.body.email,
@@ -13,15 +19,16 @@ module.exports = {
 			password: hashedPassword,
 			hash: Date.now() + Math.random().toString(16).slice(2, 14),
 			gender: req.body.gender,
-			sexuality: req.body.sexuality,
-		})
+			sexuality: req.body.sexuality
+		});
 		db.collection('user').insertOne(data, function (err, collection) {
 			if (err) throw err;
 			console.log("Record insterted successfully");
 		});
 		} catch {
 			res.redirect('/');
+			console.log('error catch');
 		}
-		console.log(data + " - $data");
+		console.log(data);
 	}
 }
