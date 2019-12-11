e7b8505c80fd6e388a3a77e7874d9017b874e4b9
JordanRheeder <JordanRheeder@gmail.com>
2019-12-11 11:59:21 +0200

    Linux installer

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install nodejs
+
 npm install -g nodemon
 npm install
 touch .env
 echo "SESSION_SECRET=MatchyMatcha
 URI = 'mongodb+srv://Admin:Epicrouter1@cluster0-fkcom.mongodb.net/matcha?retryWrites=true&w=majority'" >> ./.env
+node -v 
+npm -v 
