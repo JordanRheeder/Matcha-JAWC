3668df0cb0526cb0c79ebbdf92f41e74dddd517e
JordanRheeder <JordanRheeder@gmail.com>
2020-01-06 11:57:28 +0200

    -

--- routes/guestRoutes.js
+++ routes/guestRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express.Router();
 
 app.get('/', (req, res, next) =>{
-    res.render('admin/account.ejs');
+    res.render('admin/profile.ejs');
 }); //index
 
 module.exports = app;
