b0a0534d2d3f23aab31d4c36da792c7527670577
JordanRheeder <JordanRheeder@gmail.com>
2020-01-06 11:56:27 +0200

    Removed linux support lol

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
-sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt-get install nodejs
 
 npm install -g nodemon
 npm install
 touch .env
 echo "SESSION_SECRET=MatchyMatcha
-URI = 'mongodb+srv://Admin:Epicrouter1@cluster0-fkcom.mongodb.net/matcha?retryWrites=true&w=majority'" >> ./.env
+URI = 'mongodb+srv://Admin:<password>@cluster0-fkcom.mongodb.net/test?retryWrites=true&w=majority'" >> ./.env
 node -v 
 npm -v 
