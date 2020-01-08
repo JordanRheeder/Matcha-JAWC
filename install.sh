d423a2d1e4402d49ceb3072ffc0b9da5d5b75612
Jordan Rheeder <jordanrheeder@gmail.com>
2020-01-08 10:41:04 +0200

    Fixed install

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ npm install -g nodemon
 npm install
 touch .env
 echo "SESSION_SECRET=MatchyMatcha
-URI = 'mongodb+srv://Admin:<password>@cluster0-fkcom.mongodb.net/test?retryWrites=true&w=majority'" >> ./.env
+URI = 'mongodb+srv://Admin:<password>@cluster0-fkcom.mongodb.net/matcha?retryWrites=true&w=majority'
+EMAILPASS = 'Shattered159';
+EMAILUSER = 'matchanoreplyjawc@gmail.com';" >> ./.env
 node -v 
 npm -v 
