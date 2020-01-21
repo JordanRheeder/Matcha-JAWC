d570c94e38865deb72444a724c7d77a2acd6a9a4
Alexandra <apappas@student.wethinkcode.co.za>
2020-01-21 11:48:09 +0200

    added a placeholder profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,7 +8,4 @@ mongoose.connect(uri, {
 });
 var db=mongoose.connection;
 
-// get users pics from database
-// var userPics = db.collection('user').find({username}).project({pp:1, pp2:1, pp3:1, pp4:1, pp5:1})
-
-//or query database once for all user details and then seperate them out into the ones I want from the frontend?
+//or query database once for all user details and then seperate them out into the ones I want from the frontend or backend
