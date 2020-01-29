5e67bcc47f87eba7fb88b9645ec334c50f2cb051
JordanRheeder <JordanRheeder@gmail.com>
2020-01-29 22:48:25 +0200

    moved out the distance stuff into its own file

--- controllers/matches.js
+++ controllers/matches.js
@@ -55,48 +55,12 @@ module.exports = {
         // console.log('::queryObj:::', queryObj);
         var X = await con.db.collection('user').find( queryObj ).project({_id: 0, city: 1, pp: 1, firstname: 1, lastname: 1, username: 1, hash: 1,
           gender: 1, age: 1, bio: 1, fame: 1, ip: 1, interests: 1}).toArray();
-        for (let key in JSON.stringify(X)) {
-          if (X[key] !== 'undefined' && X[key] != null && X[key] != undefined) {
-            var propValue = X[key];
-            try {
-              console.log(key,propValue)
-              if (propValue.ip && propValue.username && propValue.ip != '' && propValue.username != '' && X[key] && propValue) {
-                ip2location.fetch(propValue.ip, function(err, res){
-                  let dog = X[key].username;
-                  console.log(dog);
-                  let theirLongitude = res.longitude;
-                  let theirLatitude = res.latitude;
-                  // calculate distances
-                  function distance(lat1, lon1, lat2, lon2) {
-                    var R = 6371; // Radius of the earth in km
-                    var dLat = (lat2 - lat1) * Math.PI / 180; // deg2rad below
-                    var dLon = (lon2 - lon1) * Math.PI / 180;
-                    var a =
-                    0.5 - Math.cos(dLat) / 2 +
-                    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-                    (1 - Math.cos(dLon)) / 2;
-                    
-                    return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-                  }
-                  
-                  let dist = distance(req.session.user.latitude, req.session.user.longitude, theirLatitude, theirLongitude)
-                  let matched = { name: X[key].username, age: X[key].age, dist: Math.trunc(dist)+" km", gender: X[key].gender}
-                  // console.log(dist + 'km');
-                  // console.log(res.longitude, res.latitude);
-                  console.log(matched);
-                {
-                  if (err) throw(err);
-                };
-              });
-          }} catch {
-            console.log('sheeeieiiiitetet')}
-          };
-      };
+        // console.log("Before ret"+returnObj)
         return (X);
     },
-    matchUsers: async function matchUsers(likedUser, loggedUser) {
-      // var alreadyLiked = await con.db.collection('matches').count({likedUser: likedUser, loggedUser: loggedUser});
-      // if (alreadyLiked == 0)
-        con.db.collection('matches').insertOne({likedUser: likedUser, loggedUser: loggedUser, blocked: false,});
-    }
+    // matchUsers: async function matchUsers(likedUser, loggedUser) {
+    //   // var alreadyLiked = await con.db.collection('matches').count({likedUser: likedUser, loggedUser: loggedUser});
+    //   // if (alreadyLiked == 0)
+    //     con.db.collection('matches').insertOne({likedUser: likedUser, loggedUser: loggedUser, blocked: false,});
+    // }
 }
