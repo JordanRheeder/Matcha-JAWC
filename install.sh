6a2d6d7a30e8b9f80a109ef76d1d39430d5e8a34
JordanRheeder <JordanRheeder@gmail.com>
2020-01-31 22:12:44 +0200

    should be hidden, but whatever

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 npm install -g nodemon
 npm install
 touch .env
-echo "SESSION_SECRET=MatchyMatcha
-URI = 'mongodb+srv://Admin:<password>@cluster0-fkcom.mongodb.net/matcha?retryWrites=true&w=majority'
-EMAILPASS = 'Shattered159';
-EMAILUSER = 'matchanoreplyjawc@gmail.com';" >> ./.env
+echo "SESSION_SECRET='MatchyMatcha'
+URI = 'mongodb+srv://Jordan:Epicrouter1@cluster0-fkcom.mongodb.net/matcha?retryWrites=true&w=majority'
+EMAILUSER = 'matchanoreplyjawc@gmail.com'
+EMAILPASS = 'Shattered159'
+MongoUser = 'jrheeder@student.wethinkcode.co.za'
+MongoPass = 'Epicrouter1'" >> ./.env
 node -v 
 npm -v 
